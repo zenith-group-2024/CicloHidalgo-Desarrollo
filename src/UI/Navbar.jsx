7d1b16ad96d33cdde4b60d2c45ba837424c8a625
nangeel <mariangel.vega@ucr.ac.cr>
2024-09-04 17:51:16 -0600

    Cambio en iconos/logo

--- src/UI/Navbar.jsx
+++ src/UI/Navbar.jsx
@@ -1,30 +1,26 @@
 import React from 'react';
+import Logo from '../assets/images/logo.svg';
+import  userIcon from '../assets/icons/userIcon.svg';
+import cartIcon from '../assets/icons/cartIcon.svg';
 
 const Navbar = () => {
     return (
         <nav className="bg-white p-2 w-full flex justify-between items-center">
             <div className="navbar-logo">
-                <img src="/src/assets/images/logo.png" alt="Logo" className="h-20 m-4" />
+                <img  src={Logo} alt="logo" className="h-20 m-4" />
             </div>
+
             <div className="flex space-x-10 text-xl">
                 <a href="#home" className="text-black font-primary font-bold hover:text-gray transform transition-transform duration-300 hover:scale-110">Inicio</a>
                 <a href="#about" className="text-black font-primary font-bold hover:text-gray transform transition-transform duration-300 hover:scale-110">Productos</a>
                 <a href="#services" className="text-black font-primary font-bold hover:text-gray transform transition-transform duration-300 hover:scale-110">Servicios</a>
                 <a href="#contact" className="text-black font-primary font-bold hover:text-gray transform transition-transform duration-300 hover:scale-110">Contenido</a>
             </div>
-            <div className="flex space-x-4 m-4">
-                <img
-                    src="/src/assets/icons/usuario.png"
-                    alt="Home Icon"
-                    className="w-10 h-10 transform transition-transform duration-300 hover:scale-110"
-                />
-                <img
-                    src="/src/assets/icons/carrito.png"
-                    alt="Coffee Icon"
-                    className="w-10 h-10 transform transition-transform duration-300 hover:scale-110"
-                />
-            </div>
 
+            <div className="flex space-x-4 m-4">
+                <img  src={userIcon} alt="logo" className="transform transition-transform duration-300 hover:scale-110" />
+                <img  src={cartIcon} alt="logo" className="transform transition-transform duration-300 hover:scale-110" />
+            </div>
         </nav>
     );
 }
