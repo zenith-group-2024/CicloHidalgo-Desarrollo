50efdd9016bd84d7b993b4193b77ac86682fd30d
nangeel <mariangel.vega@ucr.ac.cr>
2024-09-06 04:01:26 -0600

    Cambios responsive

--- src/UI/Hero.jsx
+++ src/UI/Hero.jsx
@@ -29,7 +29,7 @@ const brands = [
   },
   {
     logo: xrace,
-    hero: biciSupersix, 
+    hero: biciSupersix,
     title: "X-Race Pro",
     subtitle: "TC 15",
     description: "Diseñada para los ciclistas más exigentes.",
@@ -52,53 +52,46 @@ const Hero = () => {
 
   return (
     <div>
-      <section className="bg-white p-8">
-        <div className="max-w-6xl mx-auto flex items-center justify-between p-8">
-        
-          <div className="w-2/3">
-            <h1 className="mb-2">
-              <span className="font-bold block font-primary animate-textColor text-6xl">
-                {brands[currentIndex].title}
-              </span>
-              <span className="text-6xl block font-primary">
-                {brands[currentIndex].subtitle}
-              </span>
+      <section className="bg-white p-4 md:p-8">
+        <div className="max-w-6xl mx-auto flex flex-col md:flex-row items-center justify-between p-4 md:p-8">
+          <div className="w-full md:w-2/3 mb-8 md:mb-0">
+            <h1 className="text-3xl md:text-5xl font-bold font-primary mb-2">
+              <span className="block">{brands[currentIndex].title}</span>
+              <span className="block text-xl md:text-3xl">{brands[currentIndex].subtitle}</span>
             </h1>
-            <p className="text-lg mb-6 font-secondary">
+            <p className="text-base md:text-lg mb-6 font-secondary">
               {brands[currentIndex].description}
             </p>
             <div>
-              <button className="bg-blue text-white py-2 px-4 rounded mr-4 font-primary hover:bg-red hover:font-bold">Comprar</button>
+              <button className="bg-blue text-white py-2 px-4 rounded mr-2 md:mr-4 font-primary hover:bg-red hover:font-bold">Comprar</button>
               <button className="bg-blue text-white py-2 px-4 rounded font-primary hover:bg-red hover:font-bold">Ver más</button>
             </div>
           </div>
 
-          <div className="w-1/3">
+          <div className="w-full md:w-1/3">
             <img 
               src={brands[currentIndex].hero} 
               alt="imagen-bicicleta" 
               key={currentIndex} 
-              className="h-280 object-cover transform transition-transform duration-300 hover:scale-110" 
+              className="w-full h-auto object-cover transform transition-transform duration-300 hover:scale-100" 
             />
           </div>
         </div>
       </section>
 
-      
-      <section className="bg-gray p-8">
-        
+      <section className="bg-gray p-4 md:p-8">
         <div className="relative overflow-hidden">
-          <div className="flex flex-wrap justify-center gap-20">
+          <div className="flex flex-wrap justify-center gap-8 md:gap-12">
             {brands.map((brand, index) => (
               <div 
                 key={index} 
-                className="flex-shrink-0 w-48 cursor-pointer" 
-                onClick={() => handleLogoClick(index)} 
+                className="flex-shrink-0 w-28 h-28 md:w-36 md:h-36 cursor-pointer border-2 border-transparent transition-all duration-300 ease-in-out transform hover:scale-110 hover:border-blue-500" 
+                onClick={() => handleLogoClick(index)}
               >
                 <img
                   src={brand.logo}
                   alt={`Logo ${index}`}
-                  className="w-full h-full object-contain transition-transform duration-300 hover:scale-110"
+                  className="w-full h-full object-contain transition-transform duration-300 hover:opacity-80"
                 />
               </div>
             ))}
@@ -110,4 +103,3 @@ const Hero = () => {
 };
 
 export default Hero;
-
