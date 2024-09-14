8302aa6a0955c97da745b57ed4dda5017f9a81e6
KevinFco <kmoreravasquez@gmail.com>
2024-09-14 14:13:38 -0600

    Rama de backend principal creada

--- ciclohidalgo/database/migrations/0001_01_01_000000_create_users_table.php
+++ ciclohidalgo/database/migrations/0001_01_01_000000_create_users_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('contacto');
             $table->string('direccion');
+            $table->boolean('isAdmin');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->date('cumpleanos')->nullable();
