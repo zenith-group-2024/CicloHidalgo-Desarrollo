43dce8f9b8ed9b3b054eab4c5f70f3976e130c75
ShadowCupcak <kevinespinoza923@gmail.com>
2024-09-25 23:17:53 -0600

    Card producto modifcaciones y arreglo de filtros

--- src/UI/CardProductos.jsx
+++ src/UI/CardProductos.jsx
@@ -1,43 +1,34 @@
-import React, { useContext } from "react";
+import React from "react";
 import { motion } from "framer-motion";
 import { useInView } from "react-intersection-observer";
-import { CartContext } from './Prueba_Carrito';
 import { Link } from 'react-router-dom';
 
-const extractPrice = (priceString) => {
-  let cleanString = priceString.replace(/[^\d,.-]/g, '');
-  return parseFloat(cleanString.replace(',', '.'));
-};
-
-
-
 export const Card = (props) => {
-  
   const { ref, inView } = useInView({
     triggerOnce: true,
     threshold: 0.5,
   });
 
-
-
   return (
     <motion.div
       ref={ref}
-      className="container mx-auto p-10 border-black drop-shadow-lg rounded-md bg-[#F9F9F9]"
+      className="w-full h-72 mx-auto p-4 border-black drop-shadow-lg rounded-md bg-[#F9F9F9] flex flex-col"
     >
       <Link to={`/producto/${props.id}`}>
-     
-        <div>
-          <img src={props.img} alt={props.title} className="w-full h-auto rounded-md" />
-          <div className="text-left">
-            <h1 className="font-primary font-semibold text-xl text-black">{props.title}</h1>
-            <h2 className="font-primary font-light text-lg text-black">{props.precio}</h2>
+        <div className="flex-grow flex flex-col">
+          <div className="w-full h-48 "> {}
+            <img 
+              src={props.img} 
+              alt={props.title} 
+              className="w-full h-full object-cover rounded-md" 
+            />
+          </div>
+          <div className="flex-grow flex flex-col justify-between">
+            <h1 className="font-primary font-semibold text-lg text-black mt-2">{props.title}</h1>
+            <h2 className="font-primary font-light text-md text-black">{props.precio}</h2>
           </div>
         </div>
       </Link>
-
-      <div className="gap-8">
-      </div>
     </motion.div>
   );
 };
