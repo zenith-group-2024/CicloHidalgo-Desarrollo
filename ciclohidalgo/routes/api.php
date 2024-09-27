08d027571f2cecb724a55479917d3a48fea0db6a
neg4nin9 <eddyisaac000@gmail.com>
2024-09-27 01:46:22 -0600

    api route rename

--- ciclohidalgo/routes/api.php
+++ ciclohidalgo/routes/api.php
@@ -27,8 +27,8 @@ Route::get('/admin/users', [AdminUsersController::class, 'index']);
 Route::put('/admin/users/{id}/role', [AdminUsersController::class, 'updateRole']);
 Route::get('/user', function (Request $request) {return $request->user();}); //user() pero sin el middleware
 
-Route::get('/user/{id}', [ProfileController::class, 'getEditData']);//Añadir sanctum o middleware
-Route::put('/user/{id}', [ProfileController::class, 'updateEditData']);//Añadir sanctum o middleware
+Route::get('/user/getprofile/{id}', [ProfileController::class, 'getEditData']);//Añadir sanctum o middleware
+Route::put('/user/updateprofile/{id}', [ProfileController::class, 'updateEditData']);//Añadir sanctum o middleware
 //Route::get('/user/data', [ProfileController::class, 'getUserData']);
 //Route::get('/user/infoupdate', [ProfileController::class, 'updateUserInfo']);
 //Route::get('/user/emailupdate', [ProfileController::class, 'updateUserEmail']);
