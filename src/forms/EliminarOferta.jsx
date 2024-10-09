c1c08b7fd5874391fb33f7447f36d6a6c5c3f26b
neg4nin9 <eddyisaac000@gmail.com>
2024-10-09 06:48:49 -0600

    EliminarOferta

---
+++ src/forms/EliminarOferta.jsx
@@ -0,0 +1,128 @@
+import React from 'react';
+import { X } from 'lucide-react';
+import { useEffect, useState } from 'react';
+
+export default function EliminarOferta() {
+
+    const [productos, setProductos] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [selectedProducts, setSelectedProducts] = useState({});
+    const [backendMessage, setBackendMessage] = useState('');
+
+    useEffect(() => {
+        const fetchProductos = async () => {
+            try {
+                const response = await fetch('http://localhost:8000/api/con-descuento/all');
+                const data = await response.json();
+                setProductos(data.productos);
+            } catch (error) {
+                console.error('Error al obtener los productos:', error);
+            } finally {
+                setLoading(false);
+            }
+        };
+        fetchProductos();
+    }, []);
+
+    if (loading) {
+        return <p className="m-auto">Cargando productos...</p>;
+    }
+
+    const handleCheckboxChange = (id) => {
+        setSelectedProducts((prevSelected) => ({
+            ...prevSelected,
+            [id]: !prevSelected[id],
+        }));
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const idsToUpdate = Object.keys(selectedProducts).filter((id) => selectedProducts[id]);
+        if (idsToUpdate.length === 0) {
+            alert('Por favor, seleccione al menos un producto');
+            return;
+        }
+
+        try {
+            const response = await fetch('http://localhost:8000/api/actualizar-descuento', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ ids: idsToUpdate, descuento: 0 }), // Descuento a 0 para eliminar ofertas
+            });
+
+            if (!response.ok) {
+                console.error('Error response:', response.status, response.statusText);
+                throw new Error('Error al eliminar las ofertas');
+            }
+
+            // Actualizar productos en el frontend con descuento 0
+            const updatedProducts = productos.map((producto) => {
+                if (idsToUpdate.includes(producto.id.toString())) {
+                    return { ...producto, descuento: 0 };
+                }
+                return producto;
+            });
+
+            setProductos(updatedProducts);
+            const result = await response.json();
+            setBackendMessage(result.message);
+
+        } catch (error) {
+            console.error('Error al enviar los datos:', error);
+        }
+    };
+
+    return (
+        <>
+            <div className="min-h-screen bg-gray-100 flex justify-center py-10">
+                <div className="relative w-full max-w-4xl mx-4">
+
+                    <button className="absolute top-2 right-2">
+                        <X className="w-6 h-6 text-gray-700 hover:text-gray-900" />
+                    </button>
+
+                    <form className="bg-white p-8 shadow-md rounded-lg space-y-6" onSubmit={handleSubmit}>
+                        <h2 className="text-3xl font-semibold mb-6 text-center">Eliminar Ofertas</h2>
+
+                        <div className="grid grid-cols-4">
+                            <label className="mx-auto block text-gray-700 text-lg font-bold">Producto</label>
+                            <label className="mx-auto block text-gray-700 text-lg font-bold">Marca</label>
+                            <label className="mx-auto block text-gray-700 text-lg font-bold">Descuento actual</label>
+                            <label className="mx-auto block text-gray-700 text-lg font-bold">Eliminar Oferta</label>
+                        </div>
+
+                        {productos.length > 0 ? (
+                            productos.map((producto) => (
+                                <div className="grid grid-cols-4 " key={producto.id}>
+                                    <p className="mx-auto">{producto.nombre}</p>
+                                    <p className="mx-auto">{producto.marca}</p>
+                                    <p className="mx-auto">{producto.descuento}%</p>
+                                    <input
+                                        className="mx-auto"
+                                        type="checkbox"
+                                        checked={!!selectedProducts[producto.id]}
+                                        onChange={() => handleCheckboxChange(producto.id)}
+                                        value={producto.id}
+                                    />
+                                </div>
+                            ))
+                        ) : (
+                            <p className="text-center">No hay productos con descuento.</p>
+                        )}
+
+                        <button type="submit" className="bg-blue text-white px-4 py-2 rounded-full hover:bg-red transition w-full">
+                            Eliminar
+                        </button>
+
+                        {backendMessage && (
+                            <p className="text-center text-white bg-green-600 w-fit mx-auto p-2">{backendMessage}</p>
+                        )}
+
+                    </form>
+                </div>
+            </div>
+        </>
+    );
+}
