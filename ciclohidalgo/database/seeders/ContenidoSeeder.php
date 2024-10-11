39d173bc4d303647320c91a79f75ae53af309d00
ShadowCupcak <kevinespinoza923@gmail.com>
2024-10-11 10:37:30 -0600

    seeder de contenido arreglado

--- ciclohidalgo/database/seeders/ContenidoSeeder.php
+++ ciclohidalgo/database/seeders/ContenidoSeeder.php
@@ -17,12 +17,12 @@ class ContenidoSeeder extends Seeder
             [
                 'titulo' => '5 Productos de ciclismo feos pero que funcionan',
                 'descripcion' => '5 Productos de ciclismo feos pero que funcionan   ',
-                'video_incrustado' => 'https://www.youtube.com/watch?v=YWbh2wM8cV8&t=1s',
+                'video_incrustado' => 'https://www.youtube.com/watch?v=YWbh2wM8cV8',
             ],
             [
                 'titulo' => '7 Tendencias que No nos gustan del Ciclismo Moderno',
                 'descripcion' => '7 Tendencias que No nos gustan del Ciclismo Moderno',
-                'video_incrustado' => 'https://www.youtube.com/watch?v=H4JE6XDR6UE',
+                'video_incrustado' => 'https://www.youtube.com/watch?v=RebXydeUL8c',
             ],
             [
                 'titulo' => 'Si tuviera que elegir UNA SOLA bici, ¿cuál sería?',
