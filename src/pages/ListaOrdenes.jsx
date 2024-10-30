aaee5fb0dc4da2cb7d010cd80489198b25f4a468
neg4nin9 <eddyisaac000@gmail.com>
2024-10-29 18:49:25 -0600

    Mejoras ListaOrdenes + paginacion

--- src/pages/ListaOrdenes.jsx
+++ src/pages/ListaOrdenes.jsx
@@ -1,11 +1,16 @@
 import React, { useContext, useState, useEffect } from 'react';
 import { GlobalContext } from '../global/GlobalState';
+import Footer from '../UI/Footer';
+import Navbar from '../UI/Navbar';
 
 const ListaOrdenes = () => {
     const { state } = useContext(GlobalContext);  // Obtener el id del usuario del contexto global
     const [ordenes, setOrdenes] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
+    const [ordenSeleccionada, setOrdenSeleccionada] = useState(null);
+    const [currentPage, setCurrentPage] = useState(1);
+    const ordersPerPage = 5;
 
     useEffect(() => {
         const fetchOrdenes = async () => {
@@ -24,9 +29,26 @@ const ListaOrdenes = () => {
         };
 
         if (state.id) {
-            fetchOrdenes();  // Solo hacer la petición si el id está disponible
+            fetchOrdenes();
         }
-    }, [state.id]);  // Dependencia para ejecutar cuando el id cambie
+    }, [state.id]);
+
+    const handleVerDetalles = (ordenId) => {
+        setOrdenSeleccionada(ordenId === ordenSeleccionada ? null : ordenId);
+    };
+
+    const mostrarCampo = (label, valor) => {
+        return valor ? <p><strong>{label}:</strong> {valor}</p> : null;
+    };
+
+    const indexOfLastOrder = currentPage * ordersPerPage;
+    const indexOfFirstOrder = indexOfLastOrder - ordersPerPage;
+    const currentOrders = ordenes.slice(indexOfFirstOrder, indexOfLastOrder);
+
+    const nextPage = () => setCurrentPage((prev) => Math.min(prev + 1, Math.ceil(ordenes.length / ordersPerPage)));
+    const prevPage = () => setCurrentPage((prev) => Math.max(prev - 1, 1));
+
+
 
     if (loading) {
         return <div className="text-center text-3xl font-bold my-6">Cargando órdenes...</div>;
@@ -36,82 +58,76 @@ const ListaOrdenes = () => {
         return <div>Error: {error}</div>;
     }
 
-    const calcularSubtotal = (productos) => {
-        return productos.reduce((subtotal, producto) => {
-            return subtotal + (producto.pivot.cantidad * producto.pivot.precio);
-        }, 0);
-    };
-
-    const obtenerMetodoPago = (metodo_pago) => {
-        switch (metodo_pago) {
-            case 'sinpe':
-                return 'Sinpe Móvil - Depósito';
-            case 'efectivo':
-                return 'Efectivo';
-            case 'credomatic':
-                return 'Tasa 0 con Credomatic';
-            case 'bn':
-                return 'Sistema de apartado a 2 meses';
-            case 'mini_cuotas':
-                return 'Mini cuotas del Banco Nacional de Costa Rica';
-            default:
-                return 'Método de pago desconocido';
-        }
-    };
-
-    const eliminarOrden = async (id) => {
-        try {
-            const response = await fetch(`http://localhost:8000/api/borrar-orden/${id}`, {
-                method: 'DELETE',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-            });
-
-            if (!response.ok) {
-                throw new Error('Error al eliminar la orden');
-            }
-
-            setOrdenes(ordenes.filter(orden => orden.id !== id));
-            console.log('Orden eliminada correctamente');
-        } catch (err) {
-            setError(err.message);
-        }
-    };
-
     return (
-        <div className="min-h-screen flex flex-col bg-gray-100 p-6">
-            <h1 className="text-3xl font-bold mb-6">Tus órdenes</h1>
-            <ul className="space-y-4 w-2/3">
-                {ordenes.map((orden) => (
-                    <li className="bg-white shadow-md rounded-lg p-6" key={orden.id}>
-                        <div className="space-y-2">
-                            <p className=""><strong>ID:</strong> {orden.id}</p>
-                            <p className=""><strong>Cliente:</strong> {orden.nombre} {orden.apellido}</p>
-                            <p className=""><strong>Total:</strong> ₡{orden.total}</p>
-                            <p className=""><strong>Método de Pago:</strong> {obtenerMetodoPago(orden.metodo_pago)}</p>
-                            <p className=""><strong>Productos:</strong></p>
-                            <ul className="list-disc list-inside ml-4">
-                                {orden.productos.map((producto) => (
-                                    <div key={producto.id} className="grid grid-cols-3">
-                                        <p className="max-w-40">- {producto.nombre}</p>
-                                        <p className="max-w-40">Cantidad: {producto.pivot.cantidad}</p>
-                                        <p className="max-w-40">Precio: ₡{producto.pivot.precio}</p>
+        <>
+            <Navbar />
+
+            <div className="mx-auto min-h-screen flex flex-col p-6">
+                <h1 className="text-2xl mx-auto font-bold mb-6">Tus órdenes:</h1>
+                {ordenes.length === 0 ? (
+                    <p className="text-center text-lg font-semibold">No tienes órdenes registradas.</p>
+                ) : (
+                    <ul className="mx-auto flex flex-col space-y-4 w-2/3">
+                        {currentOrders.map((orden) => (
+                            <li className="bg-white shadow-md rounded-lg p-6" key={orden.id}>
+                                <div className="space-y-2">
+                                    <p><strong>Cliente:</strong> {orden.nombre} {orden.apellido}</p>
+                                    <p><strong>Total:</strong> ₡{orden.total}</p>
+                                    <div className='flex'>
+                                        <p className='m-auto'><strong>Productos:</strong></p>
+                                        <p className='m-auto'><strong>Cantidad:</strong></p>
                                     </div>
-                                ))}
-                            </ul>
-                            <p className="mt-4"><strong>Subtotal:</strong> ₡{calcularSubtotal(orden.productos)}</p>
-                            <button
-                                className="mt-4 px-4 py-2 bg-gray text-white rounded hover:bg-red-700"
-                                onClick={() => eliminarOrden(orden.id)}
-                            >
-                                Eliminar
-                            </button>
-                        </div>
-                    </li>
-                ))}
-            </ul>
-        </div>
+                                    <ul className="list-disc list-inside ml-4">
+                                        {orden.productos.map((producto) => (
+                                            <div key={producto.id} className="grid grid-cols-2">
+                                                <p className="ml-4">▸ {producto.nombre}</p>
+                                                <p className="m-auto">{producto.pivot.cantidad}</p>
+                                            </div>
+                                        ))}
+                                    </ul>
+                                    <button
+                                        className="mt-4 px-4 py-2 bg-blue text-white rounded hover:bg-blue-700"
+                                        onClick={() => handleVerDetalles(orden.id)}
+                                    >
+                                        {ordenSeleccionada === orden.id ? 'Ocultar detalles' : 'Ver detalles'}
+                                    </button>
+                                    {ordenSeleccionada === orden.id && (
+                                        <div className="mt-4">
+                                            <p><strong>ID del pedido:</strong> {orden.id}</p>
+                                            <p><strong>Método de Pago:</strong> {orden.metodo_pago}</p>
+                                            <p><strong>Teléfono:</strong> {orden.telefono}</p>
+                                            {mostrarCampo("Dirección", orden.direccion)}
+                                            {mostrarCampo("Provincia", orden.provincia)}
+                                            {mostrarCampo("Ciudad", orden.ciudad)}
+                                            {mostrarCampo("Código Postal", orden.codigo_postal)}
+                                        </div>
+                                    )}
+                                </div>
+                            </li>
+                        ))}
+                    </ul>
+                )}
+                {/* Controles de paginación */}
+                <div className="flex justify-center mt-4">
+                    <button
+                        className="px-4 py-2 bg-gray-300 rounded-l hover:bg-gray-400"
+                        onClick={prevPage}
+                        disabled={currentPage === 1}
+                    >
+                        Anterior
+                    </button>
+                    <span className="px-4 py-2">Página {currentPage}</span>
+                    <button
+                        className="px-4 py-2 bg-gray-300 rounded-r hover:bg-gray-400"
+                        onClick={nextPage}
+                        disabled={currentPage === Math.ceil(ordenes.length / ordersPerPage)}
+                    >
+                        Siguiente
+                    </button>
+                </div>
+            </div>
+            <Footer />
+        </>
     );
 };
 
